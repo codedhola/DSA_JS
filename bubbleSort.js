b23871857a394d4a912d02706f640cb6bc698370
codedhola <codedhola2000@gmail.com>
2023-05-10 18:39:28 +0100

    added 'bubblesort' algorithm

---
+++ bubbleSort.js
@@ -0,0 +1,19 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function bubbleSort(array) {
+    
+    for(let i = 0;i < array.length;i++){
+        // console.log(array[i])
+        for(let j = 0;j < array.length;j++){
+            console.log(array[j])
+            if(array[j] > array[j + 1]){
+                let compare = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = compare
+            }
+        }
+    }
+    return array
+}
+
+console.log(bubbleSort(numbers));
